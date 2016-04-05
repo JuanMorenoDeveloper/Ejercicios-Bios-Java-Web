595706e5811f375ae0bd15986684326b8af3d20c
jmoreno <jmoreno@PC-161642>
2016-04-05 19:54:49 -0300

    Se elimina carpeta target del versionado

--- examen-marzo-2016-solucion/target/classes/META-INF/insert-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO contacto VALUES (1, 'Jose', 'jmora@gmail.com');
-INSERT INTO contacto VALUES (2, 'Laura','lsand@gmail.com');
--- examen-marzo-2016-solucion/target/test-final-1.0.0-SNAPSHOT/WEB-INF/classes/db/insert-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO contacto (id, nombre,email) VALUES (1, 'Jose', 'jmora@gmail.com');
-INSERT INTO contacto (id, nombre,email) VALUES (2, 'Laura','lsand@gmail.com');
